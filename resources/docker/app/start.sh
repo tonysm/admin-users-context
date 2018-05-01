57eee4955ce8755e301e0c3d0f5918d19b193e76
Tony Messias <tonyzrp@gmail.com>
2018-05-01 12:48:11 -0300

    Add docker config files

---
+++ resources/docker/app/start.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+
+role=${CONTAINER_ROLE:-app}
+env=${APP_ENV:-production}
+
+echo "Attempting to connect to database..."
+until $(nc -zv db 3306); do
+    printf '.'
+    sleep 5
+done
+echo "Database ready!"
+
+if [ "$env" != "local" ]; then
+    echo "Caching configuration..."
+    (cd /var/www/html && php artisan config:cache && php artisan route:cache && php artisan view:cache)
+fi
+
+echo "Running the migrations..."
+php /var/www/html/artisan migrate -f
+
+if [ "$role" = "app" ]; then
+
+    exec apache2-foreground
+
+elif [ "$role" = "queue" ]; then
+
+    echo "Running the queue..."
+    php /var/www/html/artisan horizon
+
+elif [ "$role" = "scheduler" ]; then
+
+    while [ true ]
+    do
+        php /var/www/html/artisan schedule:run --verbose --no-interaction &
+        sleep 60
+    done
+
+else
+  echo "Could not match the container role \"$role\""
+  exit 1
+fi
